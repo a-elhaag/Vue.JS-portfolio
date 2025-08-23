2f7f6d480ba01f4177ce9683bdd423cd4ebbba1d
Anas Abuelhaag <anaselhaag@gmail.com>
2025-08-23 18:20:58 +0300

    refactor: consolidate CSS styles by removing base.css and updating imports in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { welcome } from "./data/welcome";
-import "./styles/base.css";
+import "./style.css";
 
 const app = createApp(App);
 
